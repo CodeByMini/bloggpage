8e472d5175da6e8cb6cf788f5c6ba05fedd6e25f
Daniel <daniel@lightsbymini.se>
2021-03-22 10:53:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public/'));
+
+const aboutcontent = "IoT20 is a bunch of misfits crammed togheter in a Zoom meeting talking shit on discord"
+const contactcontent = "Jag vet inte. ring nån. poolia?"
+
+const bloggposts = [];
+
+app.get('/', function(req, res) {
+    res.render('index', {bloggposts: bloggposts});
+});
+
+app.get('/about', function(req, res) {
+    res.render('about', {aboutcontent: aboutcontent});
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact', {contactcontent: contactcontent});
+});
+app.get('/compose', function(req, res) {
+    res.render('compose');
+})
+app.post('/', function(req, res){
+    console.log(req.body.composed);
+    let input={
+    title: req.body.title,
+    text: req.body.text
+    }
+    bloggposts.push(input)
+    res.redirect('/');
+});
+
+app.listen(3000, function(){
+    console.log('listening on port 3000');
+});
